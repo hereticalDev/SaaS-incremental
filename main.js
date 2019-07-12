6c9d8800562c9bf081172d7095deb2df112f6a9d
hereticaldev <hereticalDev@gmail.com>
2019-07-12 17:51:06 -0600

    WIP, added tabs

--- main.js
+++ main.js
@@ -212,3 +212,24 @@ document.getElementById("supervisorButton").addEventListener("click", supervisor
 document.getElementById("engineerButton").addEventListener("click", engineerClickHandler);
 document.getElementById("productiveEngineerButton").addEventListener("click", productiveEngineerClickHandler);
 document.getElementById("newGamePlusButton").addEventListener("click", newGamePlusClickHandler);
+
+function openDepartment(evt, departmentName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(departmentName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
